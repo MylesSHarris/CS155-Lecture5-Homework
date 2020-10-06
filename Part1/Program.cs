fe036b9fba99467bd124ab5a681439de8bb4414b
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-10-06 09:19:23 -0700

    commit

---
+++ Part1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array1 = new double[10];
+            
+            array1[0] = 1.5;
+            array1[1] = 5.2;
+            array1[2] = 3.3;
+            array1[3] = 8.0;
+            array1[4] = 7.7;
+            array1[5] = 4.5;
+            array1[6] = 3.5;
+            array1[7] = 9.9;
+            array1[8] = 0.4;
+            array1[9] = 6.3;
+
+            double[] array2 = new double[array1.Length];
+
+            for (int i=0; i<array2.Length; i++)
+            {
+                array2[i] = array1[i];
+            }
+
+            Console.WriteLine("Contents of array1:");
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(array1[i]);
+            }
+
+            Console.WriteLine("Contents of array2:");
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine(array2[i]);
+            }
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = GenerateNumbers();
+            Reverse(numbers);
+            PrintNumbers(numbers);
+        }
+
+        static void Reverse(int[] array)
+        {
+            for(int i=0; i<array.Length/2; i++)
+            {
+                int temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
+        }
+        static int[] GenerateNumbers()
+        {
+            return new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        }
+        static void PrintNumbers(int[] array)
+        {
+            for (int i=0; i<array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
